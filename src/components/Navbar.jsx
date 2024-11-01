9a53f392205f75abe33e013db13522f96afcca9d
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2024-11-01 17:24:49 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import WhatWeDo from './pages/WhatWeDo';
+import Contact from './pages/Contact';
 import { BrowserRouter, Link } from "react-router-dom";
 import { FaBars, FaXmark } from "react-icons/fa6";
 
