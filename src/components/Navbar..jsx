723ec931c1592418508bb5f5ea73e3a0ae7b0cfc
Emma Poussette <77481598+Emmapoussette@users.noreply.github.com>
2024-11-01 17:00:53 +0200

    Update Navbar..jsx

--- src/components/Navbar..jsx
+++ src/components/Navbar..jsx
@@ -1,28 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import WhatWeDo from './pages/WhatWeDo';
-import Contact from './pages/Contact';
-
-const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/whatwedo" element={<WhatWeDo />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
-    );
-};
-
-export default App;
-
-
 import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import { BrowserRouter, Link } from "react-router-dom";
